32f94bd863abadd5563de86e9dbb390ebd545375
sergio <sergio.salinas@naranya.com>
2017-10-15 20:45:13 -0500

    cambios finales

--- resources/views/admin/calendar.blade.php
+++ resources/views/admin/calendar.blade.php
@@ -91,12 +91,8 @@
                 selectHelper: true,
                 select: function(start, end, allDay) {
 
-                     alert(start);
-                    var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-                    var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+                    alert(start);
                     alert(end);
-
-
                     window.location.replace = "http://10.30.42.27/booking/public/admin/reservacions";
 
                 },
