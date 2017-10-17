f0fd6570ebb2499e70665b9d82bd244d2c33abd8
sergio <sergio.salinas@naranya.com>
2017-10-16 20:32:22 -0500

    cambios finales

--- app/Http/Controllers/Admin/EventoController.php
+++ app/Http/Controllers/Admin/EventoController.php
@@ -14,7 +14,6 @@ class EventoController extends Controller
     public function index()
     {
     	$userId = Auth::id();
-    	$ub = isset($_GET['ub'])? $_GET['ub'] : 1;
 
 		$reservacions= DB::connection('odbc')->select("SELECT 
 			a.id, 
@@ -27,8 +26,7 @@ class EventoController extends Controller
 			c.nombre_seccion
 			FROM reservaciones a  
 			JOIN users b ON a.id_usuario = b.id 
-			JOIN  seccions c ON a.id_seccion  = c.id
-			WHERE a.id_ubicacion = ".$ub." ");
+			JOIN  seccions c ON a.id_seccion  = c.id");
  	
 		foreach ($reservacions as $key ) {
 
