57ca3e31639bd806ce3fb70f470e6f4e2d6392fc
sergio <sergio.salinas@naranya.com>
2017-10-16 22:33:41 -0500

    cambios finales

--- resources/views/admin/reservacions/create.blade.php
+++ resources/views/admin/reservacions/create.blade.php
@@ -175,9 +175,10 @@
                     {!! Form::label('ubicacion', trans('quickadmin.reservacion.fields.ubicacion').'', ['class' => 'control-label']) !!}
 
                     <select name="ubicacion" id="ubicacion">
+                    <option value=".$ub_default->id." SELECTED>".$ub_default->nombre." - ".$ub_default->ciudad." - ".$ub_default->estado."</option>
                     <?php if ( $ub_from_calendar != 0 ){
                         foreach ($ubs as $ub) {
-                            if ( $ub->id == $ub_from_calendar ){
+                            if ( $ub->id == int($ub_from_calendar) ){
                                 echo "<option value='1' SELECTED>".$ub->nombre." - ".$ub->ciudad." - ".$ub->estado."</option>";
                             }
                         }
