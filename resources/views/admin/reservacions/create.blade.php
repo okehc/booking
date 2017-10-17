2911b11b170fad63657b43b8c6f7267c88889148
sergio <sergio.salinas@naranya.com>
2017-10-17 17:10:49 -0500

    changes

--- resources/views/admin/reservacions/create.blade.php
+++ resources/views/admin/reservacions/create.blade.php
@@ -2,15 +2,21 @@
 
 @section('javascript')
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link href="{{asset('res/fonts.googleapis.com.css')}}" rel="stylesheet" type="text/css">
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    
+    <link href="{{asset('res/maxcdn.bootstrapcdn.com.css')}}" rel="stylesheet" type="text/css">
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <link href="{{asset('res/maxcdn.bootstrapcdn.com2.css')}}" rel="stylesheet" type="text/css">
+
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    <!-- Jquery -->
-    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+
+
+    <script src="{{asset('res/maxcdn.bootstrapcdn.com.bootstrap.min.js')}}"></script>
+
+    <script src="{{asset('res/jquery-1.11.3.min.js')}}"></script>    
+
+
     <!-- Datepicker Files -->
     <link rel="stylesheet" href="{{asset('datePicker/css/bootstrap-datepicker3.css')}}">
     <link rel="stylesheet" href="{{asset('datePicker/css/bootstrap-standalone.css')}}">
@@ -18,12 +24,19 @@
     <!-- Languaje -->
     <script src="{{asset('datePicker/locales/bootstrap-datepicker.es.min.js')}}"></script>
  
-       <link rel="stylesheet" href="{{asset('picker/jquery.timepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('picker/jquery.timepicker.css')}}">
     <script src="{{asset('picker/jquery.timepicker.js')}}"></script>
 
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.3/jquery.timepicker.min.css">
 
-    <script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.3/jquery.timepicker.min.js"></script>
+
+    <link rel="stylesheet" href="{{asset('res/jquery.timepicker.min.css')}}">
+    <script src="{{asset('res/jquery.timepicker.min.js')}}"></script>
+
+
+
+
+
+
 
     <script>
     $(document).ready(function(){
