75e18133d4834ea34fe3357c939b64b4dcb46372
sergio <sergio.salinas@naranya.com>
2017-10-16 23:02:05 -0500

    cambios finales

--- app/Http/Controllers/Admin/ReservacionsController.php
+++ app/Http/Controllers/Admin/ReservacionsController.php
@@ -74,6 +74,8 @@ class ReservacionsController extends Controller
 
         $userId = Auth::id();
         $ub = isset($_GET['ub']) ? $_GET['ub'] : 0;
+        $date_ini = isset($_GET['start']) ? $_GET['start'] : '';
+        $date_fin = isset($_GET['end']) ? $_GET['end'] : '';
 
         $ub_default= DB::connection('odbc')->selectOne("SELECT a.id, a.nombre, a.ciudad, a.estado FROM ubicaciones a JOIN users b ON a.id = b.ubicacion WHERE b.id = ".$userId." ");
 
